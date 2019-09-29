b90a4ccd97eec1bd009339d4892640aa4229edb8
Maya Liberman <mayaliberman1@gmail.com>
2019-09-29 20:13:40 +0300

    Updates

---
+++ precache-manifest.37be9b099a6d0494c48a6221f2ee86e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54b223bbc0f147c7c87e15d957ff185",
+    "url": "/unit-7-react-photo-gallery/index.html"
+  },
+  {
+    "revision": "26793d4a7b07ade65b31",
+    "url": "/unit-7-react-photo-gallery/static/css/main.b81e1866.chunk.css"
+  },
+  {
+    "revision": "bff822a030613bd09ee6",
+    "url": "/unit-7-react-photo-gallery/static/js/2.07ea4827.chunk.js"
+  },
+  {
+    "revision": "26793d4a7b07ade65b31",
+    "url": "/unit-7-react-photo-gallery/static/js/main.2df7c46c.chunk.js"
+  },
+  {
+    "revision": "ab7a1df93761ebcbb7fd",
+    "url": "/unit-7-react-photo-gallery/static/js/runtime-main.5c9a1da0.js"
+  }
+]);
